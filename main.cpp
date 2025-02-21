e7e4a8a93e66470603c2ad4074e75c446ed96e8f
tkmaher <tkmaher@umich.edu>
2025-02-21 16:54:55 -0500

    latest changes

--- main.cpp
+++ main.cpp
@@ -5,11 +5,20 @@
 #include <thread>
 #include <mutex>
 
+std::mutex bufferLock;
+
 void parse(char * buffer, size_t &pagesize) {
    std::lock_guard<std::mutex> lock(bufferLock);
    HtmlParser parser( buffer, pagesize );
    for (auto i : parser.links) 
       std::cout << i.URL << std::endl;
+   std::lock_guard<std::mutex> unlock(bufferLock);
+}
+
+void crawl(ParsedUrl &url, char *buffer, size_t &pageSize) {
+   std::lock_guard<std::mutex> lock(bufferLock);
+   crawl(url, buffer, pageSize);
+   std::lock_guard<std::mutex> unlock(bufferLock);
 }
 
 int main(int argc, char* argv[]) {
@@ -18,7 +27,6 @@ int main(int argc, char* argv[]) {
       return 1;
    }
 
-   std::mutex bufferLock;
 
    std::string url = argv[1];
    ParsedUrl purl(url);
@@ -26,8 +34,10 @@ int main(int argc, char* argv[]) {
    char buffer[2000000]; //how big should the buffer be?
    size_t pageSize;
 
-   std::thread t1(crawl, purl, buffer, pageSize, bufferLock);
-   std::thread t2(parse, buffer, pageSize);
+   std::thread t1(crawl, purl, pageSize);
+   std::thread t2(parse, pageSize);
+   t1.join();
+   t2.join();
 
    return 0;
 }
